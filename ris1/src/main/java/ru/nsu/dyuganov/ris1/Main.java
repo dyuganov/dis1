29e7097ff325347accdf907ce31523c0cc2acdf7
Nikita <dyuganov.n@gmail.com>
2023-02-12 02:48:48 +0700

    project dependencies and logger init

---
+++ ris1/src/main/java/ru/nsu/dyuganov/ris1/Main.java
@@ -0,0 +1,12 @@
+package ru.nsu.dyuganov.ris1;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        log.debug("Logs test");
+        System.out.println("Hello, World of rice!");
+    }
+}
